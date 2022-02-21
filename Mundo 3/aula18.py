10a65448d2f42c93abe4f63035a586eb417f09fd
Carlos <iamcarloseduardo@hotmail.com>
2022-02-21 11:05:35 -0300

    Exercicios

---
+++ Mundo 3/aula18.py
@@ -0,0 +1,32 @@
+#teste = list()
+#teste.append('Gustavo')
+#teste.append(40)
+#galera = list()
+#galera.append(teste[:])
+#teste[0] = 'Maria'
+#teste[1] = 22
+#galera.append(teste[:])
+#print(galera)
+#galera = [['João', 19], ['Ana', 33],['Joaquim', 13], ['Maria', 45]]
+#print(galera[0])
+#for per in galera:
+#    print(f'{per[0]} tem {per[1]} anos.')
+#galera = list()
+#dado = list()
+#maior = menor = 0
+#for cont in range(0, 4):
+#    dado.append(str(input('Nome: ')))
+#    dado.append(int(input('Idade: ')))
+#    galera.append(dado[:])
+#    dado.clear()
+
+#for per in galera:
+#    if per[1] >= 21:
+#        print(f'{per[0]} é maior de idade')
+#        maior += 1
+#    else:
+#        print(f'{per[0]} é menor de idade')
+#        menor += 1
+#print('-' * 30)
+#print(f'''Temos {maior} maiores
+#E {menor} menor de idade.''')
---
+++ Mundo 3/ex0087.py
@@ -0,0 +1,17 @@
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+somap = maior = 0
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        matriz[linha][coluna] = int(input(f'Digite um valor [{linha}, {coluna}] '))
+        if matriz[linha][coluna] % 2 == 0:
+            somap += matriz[linha][coluna]
+print('-' * 30)
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        print(f'[ {matriz[linha][coluna]:^5} ]', end='')
+    print()
+somat = matriz[0][2] + matriz[1][2] + matriz[2][2]
+print('-' * 30)
+print(f'A soma dos valores pares é {somap}')
+print(f'A soma dos valores da terceira coluna é {somat}')
+print(f'O maior valor da segunda linha é {max(matriz[1])}')
