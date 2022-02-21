8496f3a33ad1536d45c174811237276fe8dc3561
Carlos <iamcarloseduardo@hotmail.com>
2022-02-21 11:07:56 -0300

    Exercicios

--- Mundo 3/ex0087.py
+++ Mundo 3/ex0087.py
@@ -1,5 +1,5 @@
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-somap = maior = 0
+somap = maior = somat = 0
 for linha in range(0, 3):
     for coluna in range(0, 3):
         matriz[linha][coluna] = int(input(f'Digite um valor [{linha}, {coluna}] '))
@@ -11,6 +11,8 @@ for linha in range(0, 3):
         print(f'[ {matriz[linha][coluna]:^5} ]', end='')
     print()
 somat = matriz[0][2] + matriz[1][2] + matriz[2][2]
+#for linha in range(0, 3):
+#    somat += matriz[linha][2]
 print('-' * 30)
 print(f'A soma dos valores pares é {somap}')
 print(f'A soma dos valores da terceira coluna é {somat}')
