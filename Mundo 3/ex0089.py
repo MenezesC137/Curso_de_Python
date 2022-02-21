f856ea5356ff973b10786c8ae6d6a7274b2288f0
Carlos <iamcarloseduardo@hotmail.com>
2022-02-21 13:44:27 -0300

    Exercicios

--- Mundo 3/ex0089.py
+++ Mundo 3/ex0089.py
@@ -1,26 +1,31 @@
 ficha = list()
 linha = '-' * 25
 while True:
+#variaveis
     nome = str(input('Nome: '))
     n1 = float(input('Nota 1: '))
     n2 = float(input('Nota 2: '))
     media = (n1 + n2) / 2
     ficha.append([nome, [n1, n2], media])
+#1ªCondição de parada
     sn = ' '
     while sn not in 'SN':
         sn = str(input('Quer continuar? [S/N]')).upper().strip()[0]
     if sn == 'N':
         break
+#Menu
 print(linha)
 print(f'{"No.":<4}{"Nome":<12}{"Média":>7}')
 print(linha)
 for indice, aluno in enumerate(ficha):
     print(f'{indice:<4}{aluno[0]:<12}{aluno[2]:>5.1f}')
+#2ªCondição de parada
 while True:
     print(linha)
     opc = int(input('Mostrar notas de qual aluno? [999 interrompe]:'))
     if opc == 999:
         print('Finalizando...')
         break
+#Mostrando os dados do aluno solicitado
     if opc <= len(ficha) - 1:
         print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
