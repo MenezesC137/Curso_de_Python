550d5ab7f1044ef743c9ebfa8e8248dc14204649
Carlos <iamcarloseduardo@hotmail.com>
2022-02-24 16:52:18 -0300

    Exercicios

--- Mundo 3/ex0090.py
+++ Mundo 3/ex0090.py
@@ -1,2 +1,17 @@
+linha = '-' * 30
 aluno = dict()
-
+nome = str(input('Nome: '))
+aluno['nome'] = nome
+media = float(input(f'Media do {nome}: '))
+aluno['media'] = media
+print(linha)
+for key, val in aluno.items():
+    print(f' - {key} é igual a {val}')
+if media <= 5:
+    print(f' - Situação é igual a Reprovado')
+elif 5 > media <= 7:
+    print(f' - Situação é igual a Recuperação')
+elif 7 > media <= 10:
+    print(f' - Situação é igual a Aprovado')
+else:
+    sit = 'Nota digitada errada'
